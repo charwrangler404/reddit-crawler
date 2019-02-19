991cd718eb28301fdcfcee6291018e53d8c9ced4
charwrangler404 <charwrangler404@gmail.com>
2019-02-18 20:36:28 -0600

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -3,17 +3,8 @@
 import praw
 
 def reply(comment):
+    ### Place comment reply here
     reply_text = """
-- If you feel you are a danger to yourself or others, please call 911 or your local emergency number
-- US National Suicide Prevention Hotline: 1-800-273-8255
-- Crisis text line: 741-741
-- Veterans Crisis Line: 1-800-273-8255, press 1
-
-*Fully automated*
-
-*Please submit bug reports at /r/CrisisBot*
-
-*Contact /u/alkaloid_android with major problems*
 """
     comment.reply(reply_text)
 
@@ -21,7 +12,7 @@ def summon_search(subreddit):
     ### Grab comments as they appear
     for comment in subreddit.stream.comments(skip_existing=True):
         # Check for the bat signal
-        if str(comment.body.lower()[0:10]) == '!crisisbot':
+        if str(comment.body.lower()[0:10]) == '':
             ### Reply to the comment
             reply(comment)
             ### Print comment body to the console - helps track that the scipt
@@ -41,7 +32,7 @@ def main():
     reddit = praw.Reddit(client_id=id,client_secret=secret,user_agent=agent,username=name,password=pw)
 
     ### Define subreddit
-    subreddit = reddit.subreddit('SchizoaffectiveBot')
+    subreddit = reddit.subreddit('')
 
     ### Summon the bot!
     summon_search(subreddit)
